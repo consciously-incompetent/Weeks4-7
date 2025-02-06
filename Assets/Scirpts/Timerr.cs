34c9bc5904ec0d723fd96bd26654e062983a763c
Otis Murphy <murphyot@shernet.sheridancollege.ca>
2025-02-06 13:39:57 -0500

    visualizing timers with different slider settings

---
+++ Assets/Scirpts/Timerr.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timerr : MonoBehaviour
+{
+    Slider slider;
+    float t;
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    
+    void Update()
+    {
+        t += Time.deltaTime;
+        slider.value = t % slider.maxValue;
+
+    }
+}
